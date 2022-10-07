5a6e352ad3ada9b69b6f67ec5219658719aa39c9
Gryv2 <115256395+Gryv2@users.noreply.github.com>
2022-10-07 19:57:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,16 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
-global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
+global.Owner = ['919755062790']
+global.OwnerNumber = ['919755062790']
+global.ownertag = ['919755062790']
+global.BotName = "Gryv2"
+global.packname = "Gryv2"
+global.author = "By: Gryv2"
+global.OwnerName = "nikhil"
+global.BotSourceCode = "https"
+global.SupportGroupLink = 
+
 global.sessionName = "session"
 
 // Prefix //
@@ -23,11 +24,11 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "Madhya pradesh,India"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
+global.website ="
 global.lolhuman = "KaysaS"
 
 
